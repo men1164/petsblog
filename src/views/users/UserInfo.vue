4f6fcbdc06940525bb6720a68ee236eda16e8a1f
men1164 <41180013+men1164@users.noreply.github.com>
2021-10-24 14:01:15 +0700

    init user's info page

---
+++ src/views/users/UserInfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-white max-w-4xl h-auto rounded-2xl shadow-xl block mx-auto my-0 p-20">
+        <img :src="user.photoURL">
+        <p>{{ user.displayName }}</p>
+        <p>{{ user.email }}</p>
+    </div>
+</template>
+
+<script>
+import getUser from '@/composables/getUser'
+
+export default {
+    setup() {
+        const { user } = getUser()
+
+        return { user }
+    }
+}
+</script>
+
+<style>
+
+</style>
