404239edd05fefcba18ec73691c1540af43bba8d
men1164 <41180013+men1164@users.noreply.github.com>
2021-10-26 18:54:12 +0700

    navbar selected transition

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="navbar py-4 px-2 bg-primary-green bg-opacity-75 shadow-lg sticky top-0 z-50">
-    <nav class="flex items-center h-10 max-w-7xl mx-auto my-0">
+    <nav class="flex items-center h-10 max-w-7xl mx-auto my-0 text-white">
       <p class="font-bold text-xl text-white">Navbar</p>
+      <div class="ml-14 font-medium">
+        <router-link class="text-gray-100 transition duration-300 ease-in-out hover:text-white focus:text-white" :to="{ name: 'UserInfo' }">Profile</router-link>
+        <!-- <router-link>Your Pet</router-link>
+        <router-link>Following</router-link> -->
+      </div>
       <div class="ml-auto font-normal text-base">
         <div v-if="user" class="flex flex-row items-center">
           <p class="mr-5">{{ user.displayName }}</p>
-          <button class="rounded-xl bg-gray-200 border-0 cursor-pointer inline-block py-2 px-3 shadow-md mr-2" @click="handleLogout">Log out</button>
+          <button class="rounded-xl text-black bg-gray-200 border-0 cursor-pointer inline-block py-2 px-3 shadow-md mr-2" @click="handleLogout">Log out</button>
         </div>
         <!-- <div v-else>
           <router-link :to="{ name: 'Login' }" class="rounded-xl bg-gray-200 border-0 cursor-pointer inline-block py-2 px-3 shadow-md mr-2">Login</router-link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontWeight: ['hover', 'focus'],
+      transitionDuration: ['hover', 'focus'],
+    },
   },
   plugins: [],
 }
