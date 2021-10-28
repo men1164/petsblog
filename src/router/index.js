0b6f813b79c299661e7621b6599713b5f68864a3
sjnoolek <62029557+sjnoolek@users.noreply.github.com>
2021-10-28 16:55:09 +0700

    Add route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Signup from '../views/auth/Signup.vue'
 import Login from '../views/auth/Login.vue'
 import UserInfo from '../views/users/UserInfo.vue'
+import CreateBlog from '../views/blogs/CreateBlog.vue'
 
 const routes = [
   {
@@ -19,6 +20,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/createblog',
+    name: 'CreateBlog',
+    component: CreateBlog
   },
   {
     path: '/userinfo',
