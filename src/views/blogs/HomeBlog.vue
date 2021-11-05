e522a8b72e7f44900d43f3c604fc74e6f46facf9
men1164 <41180013+men1164@users.noreply.github.com>
2021-11-05 20:34:48 +0700

    style typo fixed

--- src/views/blogs/HomeBlog.vue
+++ src/views/blogs/HomeBlog.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="ml-20 mt-15">
-        <p class="text-white font-semobold text-lg">News from our Veterinarian!</p>
+        <p class="text-white font-semibold text-lg">News from our Veterinarian!</p>
     </div>
     <div class="flex px-20 py-4 overflow-x-auto">
         <BlogCardSM />
@@ -12,7 +12,7 @@
         <BlogCardSM />
     </div>
     <div class="ml-20 mt-10">
-        <p class="text-white font-semobold text-lg">New Blog from our members!</p>
+        <p class="text-white font-semibold text-lg">New Blog from our members!</p>
     </div>
     <div class="flex px-20 pt-4 pb-10 w-full overflow-x-auto">
         <PetCard />
