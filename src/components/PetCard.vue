ae4df9727c5b93d8cdcfbb8ac94e9c141ced6287
men1164 <41180013+men1164@users.noreply.github.com>
2021-11-05 20:32:58 +0700

    petcard frontent complete (waiting for backend)

--- src/components/PetCard.vue
+++ src/components/PetCard.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="card bg-white rounded-3xl min-w-cardWidth min-h-cardHeight mb-7 mr-7 shadow-xl">
-
+        <div class="flex flex-col w-full h-full">
+            <div class="w-full h-2/3 relative">
+                <img class="absolute top-1 right-3 h-10 w-10" src="@/assets/new.png">
+                <img class="h-full w-full object-cover rounded-t-3xl" src="@/assets/demo-petcard.jpg">
+            </div>
+            <div class="w-full h-1/3 text-left pl-5 pt-3">
+                <p class="font-semibold">{ Pet's Name }</p>
+                <p class="mt-2 text-sm">Owner: { displayName }</p>
+                <p class="text-sm">Gender: { Gender }</p>
+                <p class="text-sm">Breed: { Breed }</p>
+            </div>
+        </div>
     </div>
 </template>
 
