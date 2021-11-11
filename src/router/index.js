4d82257c10e0a0dde164ffa6892fa2a20c8a53e7
men1164 <41180013+men1164@users.noreply.github.com>
2021-11-11 11:53:57 +0700

    add router function, redirect to explore page when logged in user try to route to home

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,24 @@ import UserInfo from '../views/users/UserInfo.vue'
 import CreateBlog from '../views/blogs/CreateBlog.vue'
 import Blogview from '../views/blogs/Blogview.vue'
 
+import { projectAuth } from '../firebase/config'
+
+const isLoggedIn = (to, from, next) => {
+    let user = projectAuth.currentUser
+    if(user) {
+        next({ name: 'Explore' })
+    }
+    else {
+        next()
+    }
+}
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: isLoggedIn
   },
   {
     path: '/explore',
