7f892fec6263090551352a910c7ce2271dff73b6
men1164 <41180013+men1164@users.noreply.github.com>
2021-11-12 12:58:03 +0700

    like button created

--- src/views/blogs/Blogview.vue
+++ src/views/blogs/Blogview.vue
@@ -22,6 +22,20 @@
                     <input type="text" class="hidden">
                 </span> -->
             </div>
+            <!-- unlike state -->
+            <div class="mx-16 mb-5 flex items-center">
+                <svg @click="handleLike" class="h-8 w-8 text-gray-300 hover:text-blue-300 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                </svg>
+                <p class="ml-1">48</p>
+            </div>
+            <!-- like state -->
+            <!-- <div class="mx-16 mb-5 flex items-center">
+                <svg @click="handleLike" class="h-8 w-8 text-blue-400 hover:text-blue-300 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                </svg>
+                <p class="ml-1">48</p>
+            </div> -->
         <!-- ! Comment Section -->
             <div class="mx-16">
                 <p class="font-bold text-2xl">Comments</p>
@@ -44,7 +58,11 @@ export default {
         const { blog } = getBlogDetail('petBlog', props.blogId)
         const { pet } = getPetDetail('petDetail', props.petId)
 
-        return { blog, pet }
+        const handleLike = () => {
+            console.log('like!')
+        }
+
+        return { blog, pet, handleLike }
     }
 }
 </script>
